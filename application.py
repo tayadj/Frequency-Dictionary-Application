947983d603ebb9d09c11d65cfe8dd2b286537593
tayadj <144615941+tayadj@users.noreply.github.com>
2024-09-27 10:29:14 +0300

    Add files via upload

--- application.py
+++ application.py
@@ -5,34 +5,56 @@ import pandas
 import sys
 
 
+class QNumericTableWidgetItem(QTableWidgetItem):
+    def __lt__(self, other):
+        if isinstance(other, QTableWidgetItem):
+            try:
+                return float(self.text()) < float(other.text())
+            except ValueError:
+                pass
+        return super().__lt__(other)
+
+
 
 
 
 class Window(QMainWindow):
 
-    language        = 3
-    dictionary      = None
+    language                    = 1
+    dictionary                  = None
 
-    menu            = None
-    menuView        = None
-    menuEdit        = None
-    menuSettings    = None
-    view            = None
-    viewDictionary  = None
-    viewSettings    = None
+    menu                        = None
+    menuView                    = None
+    menuEdit                    = None
+    menuSettings                = None
+    view                        = None
+    viewDictionary              = None
+    viewSearch                  = None
+    viewSettings                = None
+    viewSettingsLanguageHeader  = None
+    viewSettingsLanguageLine    = None
+    viewSettingsLanguageOption  = None
+    viewSettingsLanguageEnglish = None
+    viewSettingsLanguageSpanish = None
+    viewSettingsLanguageRussian = None
+
 
 
 
     def errorMessage(self, error):
         print("Error " + str(error)+":")
         match error:
-            case 101:
+            case 1025:
                 print("Unknown language")
+            case 1026:
+                print("Setting language is wrong")
             case _:
                 print("Unknown error")
 
     def fillViewDictionary(self):
         self.viewDictionary.clear()
+        self.viewDictionary.setRowCount(0)
+        self.viewDictionary.setSortingEnabled(False)
         self.viewDictionary.show()
         self.viewDictionary.setRowCount(self.dictionary.shape[0])
         self.viewDictionary.setHorizontalHeaderItem(0, QTableWidgetItem("Wordform"))
@@ -41,7 +63,8 @@ class Window(QMainWindow):
         self.viewDictionary.horizontalHeaderItem(1).setFont(QFont("Segoe UI Black",8))
         for i in range(self.dictionary.shape[0]):
             self.viewDictionary.setItem(i,0, QTableWidgetItem(str(self.dictionary.iat[i,0])))
-            self.viewDictionary.setItem(i,1, QTableWidgetItem(str(self.dictionary.iat[i,1])))
+            self.viewDictionary.setItem(i,1, QNumericTableWidgetItem(str(self.dictionary.iat[i,1])))
+        self.viewDictionary.setSortingEnabled(True)
 
     def loadDictionary(self):
         match self.language:
@@ -52,7 +75,7 @@ class Window(QMainWindow):
             case 3:
                 self.dictionary = pandas.read_csv("../dict_ru.csv")
             case _:
-                self.errorMessage(101)
+                self.errorMessage(1025)
 
     def clicked_menuView(self):
         self.viewDictionary.show()
@@ -63,6 +86,18 @@ class Window(QMainWindow):
     def clicked_menuSettings(self):
         self.viewDictionary.hide()
         self.viewSettings.show()
+
+    def buttonClicked_viewSettingsLanguageOption(self, option):
+        match option.text():
+            case "ENGLISH":
+                self.language = 1
+            case "SPANISH":
+                self.language = 2
+            case "RUSSIAN":
+                self.language = 3
+            case _:
+                self.errorMessage(1026)
+
 
     def __init__(self, parent=None):
 
@@ -105,22 +140,61 @@ class Window(QMainWindow):
                                         "}")
 
         self.view = QGroupBox(self)
+        self.view.setObjectName("view")
         self.view.setGeometry(240, 20, 460, 500)
+        self.view.setStyleSheet("#view {"
+                                "   border: none;"
+                                "}")
 
         self.viewDictionary = QTableWidget(self.view)
         self.viewDictionary.setObjectName("viewDictionary")
         self.viewDictionary.setGeometry(20, 20, 420, 460)
         self.viewDictionary.setColumnCount(2);
-        self.viewDictionary.setColumnWidth(0, 205)
-        self.viewDictionary.setColumnWidth(1, 205)
+        self.viewDictionary.setColumnWidth(0, 203)
+        self.viewDictionary.setColumnWidth(1, 203)
         self.viewDictionary.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.viewDictionary.verticalHeader().setVisible(False)
+        self.viewDictionary.setSortingEnabled(True)
         self.viewDictionary.hide()
 
         self.viewSettings = QGroupBox(self.view)
         self.viewSettings.setObjectName("viewSettings")
         self.viewSettings.setGeometry(20, 20, 420, 460)
+        self.viewSettings.setStyleSheet("#viewSettings {"
+                                        "   border-radius: 10px;"
+                                        "   background-color: #f8f8f8;"
+                                        "}")
         self.viewSettings.hide()
+
+        self.viewSettingsLanguageHeader = QLabel(self.viewSettings)
+        self.viewSettingsLanguageHeader.setGeometry(20, 20, 120, 20)
+        self.viewSettingsLanguageHeader.setFont(QFont("Segoe UI Black",12))
+        self.viewSettingsLanguageHeader.setText("LANGUAGE")
+
+        self.viewSettingsLanguageLine = QFrame(self.viewSettings)
+        self.viewSettingsLanguageLine.setGeometry(20, 44, 380, 2)
+        self.viewSettingsLanguageLine.setFrameShape(QFrame.HLine)
+        self.viewSettingsLanguageLine.setFrameShadow(QFrame.Sunken)
+
+        self.viewSettingsLanguageEnglish = QRadioButton(self.viewSettings, "English")
+        self.viewSettingsLanguageEnglish.setGeometry(20, 60, 120, 20)
+        self.viewSettingsLanguageEnglish.setFont(QFont("Segoe UI Black",10))
+        self.viewSettingsLanguageEnglish.setText("ENGLISH")
+        self.viewSettingsLanguageEnglish.setChecked(True)
+        self.viewSettingsLanguageSpanish = QRadioButton(self.viewSettings, "Spanish")
+        self.viewSettingsLanguageSpanish.setGeometry(20, 90, 120, 20)
+        self.viewSettingsLanguageSpanish.setFont(QFont("Segoe UI Black",10))
+        self.viewSettingsLanguageSpanish.setText("SPANISH")
+        self.viewSettingsLanguageRussian = QRadioButton(self.viewSettings, "Russian")
+        self.viewSettingsLanguageRussian.setGeometry(20, 120, 120, 20)
+        self.viewSettingsLanguageRussian.setFont(QFont("Segoe UI Black",10))
+        self.viewSettingsLanguageRussian.setText("RUSSIAN")
+
+        self.viewSettingsLanguageOption = QButtonGroup(self.viewSettings)
+        self.viewSettingsLanguageOption.addButton(self.viewSettingsLanguageEnglish)
+        self.viewSettingsLanguageOption.addButton(self.viewSettingsLanguageSpanish)
+        self.viewSettingsLanguageOption.addButton(self.viewSettingsLanguageRussian)
+        self.viewSettingsLanguageOption.buttonClicked.connect(self.buttonClicked_viewSettingsLanguageOption)
 
 
 
